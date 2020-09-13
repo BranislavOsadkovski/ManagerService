fbf20225a79a37e77d64d57e82b295b5e48d8495
Branislav <Osadkovski>
2020-09-13 11:52:17 +0200

    setting up context

--- src/main/java/com/school/listeners/ServletContextHandler.java
+++ src/main/java/com/school/listeners/ServletContextHandler.java
@@ -3,15 +3,13 @@ package com.school.listeners;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.Enumeration; 
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;  
 
-import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
-import com.school.objects.Student;
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread; 
 import com.school.util.ApplicationManager;
 import com.school.util.StudentJDBCTemplate;
  
@@ -27,10 +25,6 @@ public class ServletContextHandler implements ServletContextListener {
 		this.studentTemplate = (StudentJDBCTemplate)ApplicationManager.getSpringAppContext().getBean("studentJDBCTemplate");
 		context.setAttribute("studentTemplate", studentTemplate);
 
-		List<Student> list = studentTemplate.getAllStudents();
-		for(Student s : list ) {
-			System.out.printf("id: %s name: %s age: %s email: %s image: %s \n",s.getId(),s.getName(),s.getAge(),s.getEmail(),s.getImage());
-		}
 	}
 
 	@Override
@@ -49,8 +43,8 @@ public class ServletContextHandler implements ServletContextListener {
 	                // deregistration failed, might want to do something, log at the very least
 	            }
 	        } 
-	        try {
-	        AbandonedConnectionCleanupThread.uncheckedShutdown();
+	        try { 
+	        	AbandonedConnectionCleanupThread.uncheckedShutdown();
 	        }catch(Exception e){e.printStackTrace();}
 	}    
 	
