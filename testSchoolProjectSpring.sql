6a3a2fd0000d918fd16a68ed0127b7831117bfad
Branislav <Osadkovski>
2020-09-25 23:49:25 +0200

    projectSQLscript

---
+++ testSchoolProjectSpring.sql
@@ -0,0 +1,47 @@
+DROP DATABASE IF EXISTS `test`;
+CREATE DATABASE `test`; 
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50) NOT NULL,
+age int NOT NULL,email varchar(30) NOT NULL,image mediumblob)engine=INNODB;
+
+DROP PROCEDURE IF EXISTS `getRecord`;
+DELIMITER $$
+CREATE PROCEDURE  `getRecord` (
+IN in_id INTEGER, 
+OUT out_name VARCHAR(20),
+OUT out_age  INTEGER,
+OUT out_email VARCHAR(50),
+OUT out_image BLOB)
+BEGIN
+   SELECT name, age, email,image
+   INTO out_name, out_age,out_email,out_imagestudent
+   FROM Student where id = in_id;
+END $$
+DELIMITER ;  
+
+DROP PROCEDURE IF EXISTS `getRecordByName`;
+DELIMITER $$
+CREATE PROCEDURE  `getRecordByName` (
+IN in_name VARCHAR(20),
+OUT out_id INTEGER,
+OUT out_age  INTEGER,
+OUT out_email VARCHAR(50),
+OUT out_image BLOB)
+BEGIN
+   SELECT id, age, email,image
+   INTO out_id, out_age,out_email,out_image
+   FROM Student where name= in_name;
+END $$
+DELIMITER ;  
+
+DROP FUNCTION IF EXISTS `get_student_image`;
+DELIMITER $$
+CREATE FUNCTION get_student_image(in_id INTEGER)
+RETURNS MEDIUMBLOB
+BEGIN
+DECLARE out_image MEDIUMBLOB;
+SELECT image INTO out_image FROM student WHERE id=in_id;
+RETURN out_image;
+END $$
+DELIMITER ;
+
