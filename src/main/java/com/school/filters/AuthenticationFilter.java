3d8debb155c2c1f00aed4da8fc31d217b762fca2
Branislav <Osadkovski>
2020-10-01 23:35:14 +0200

    java doc update

--- src/main/java/com/school/filters/AuthenticationFilter.java
+++ src/main/java/com/school/filters/AuthenticationFilter.java
@@ -12,41 +12,51 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 
-/**AuthenticationFilter is used to log the resource that is requested by the client each time the Filter.doFilter() method is called
+/**
+ * AuthenticationFilter is used to log the resource that is requested by the
+ * client each time the Filter.doFilter() method is called
+ * 
  * @author Branislav
  *
  */
-public class AuthenticationFilter implements Filter{
-	
+public class AuthenticationFilter implements Filter {
+
 	private final static Logger logger = Logger.getLogger(AuthenticationFilter.class);
+
 	/**
-	 *  Called by the web container to indicate to a filter that it is being placed into service.
+	 * Called by the web container to indicate to a filter that it is being placed
+	 * into service.
+	 * 
 	 * @param cfg
 	 */
 	@Override
 	public void init(FilterConfig cfg) {
 		logger.info("AuthenticationFilter initialized");
 	}
-	
+
 	/**
-	 * The doFilter method of the Filter is called by the container each time a request/response pair is passed 
-	 * through the chain due to a client request for a resource at the end of the chain
-	 *@param request
-	 *@param response
-	 *@param chain
+	 * The doFilter method of the Filter is called by the container each time a
+	 * request/response pair is passed through the chain due to a client request for
+	 * a resource at the end of the chain
+	 * 
+	 * @param request
+	 * @param response
+	 * @param chain
 	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String uri = httpRequest.getRequestURI();
-		logger.info("Requested Resourse : "+uri);
-		
+		logger.info("Requested Resourse : " + uri);
+
 		chain.doFilter(request, response);
-		
+
 	}
-	/** 
-	 * Called by the web container to indicate to a filter that it is being taken out of service.
+
+	/**
+	 * Called by the web container to indicate to a filter that it is being taken
+	 * out of service.
 	 */
 	@Override
 	public void destroy() {
